ebe7a2c28060a27dcd54f4ab7a563cb180aa2253
pooyan <pooyan.jbn@gmail.com>
2024-02-04 20:25:45 +0330

    service section full complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,42 +2,61 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    screens:{
-      sm:"576px",
-      md:"768px",
-      lg:"992px",
-      xl:"1200px",
-     "2xl":"1320px",
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1320px",
     },
     container: {
       center: true,
-      screens:{
-        sm:"576px",
-        md:"768px",
-        lg:"992px",
-        xl:"1200px",
-       "2xl":"1320px",
+      screens: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1320px",
       },
     },
     extend: {
-      fontFamily:{
-        dana:["dana"],
-        kalameh:["kalameh"],
+      fontFamily: {
+        dana: ["dana"],
+        kalameh: ["kalameh"],
       },
-      colors :{
-        // Brown
-        primaryBrownOne: '#663518' ,
-        // Brown Light
-        primaryBrownLightOne: '#FFF7ED',
-        primaryBrownLightTwo: '#DDCDAF',
-        // Green Light
-        primaryGreenLightOne: '#A2B27D',
-        // Black
-        primaryBlackTwo: '#222222',
-        primaryBlackSeven: '#757575'
-      }
+
+      colors: {
+        primary: {
+          brown: {
+            100: "#663518",
+            light: {
+              100: '#FFF7ED',
+              200: '#DDCDAF',
+            },
+          },
+          green: {
+            100: '#A2B27D',
+          },
+          black: {
+            200: '#222222',
+            700: '#757575',
+          },
+        },
+      },
+
+      // colors :{
+      //   // Brown
+      //   primary-brown-100: '#663518' ,
+      //   //Light Brown 
+      //   primaryBrownLightOne: '#FFF7ED',
+      //   : '#DDCDAF',
+      //   //Light Green 
+      //   primary-green-100: '#A2B27D',
+      //   // Black
+      //   primaryBlackTwo: '#222222',
+      //   primary-black-700: '#757575'
+      // }
     },
   },
-  plugins: [], 
+  plugins: [],
 }
-
